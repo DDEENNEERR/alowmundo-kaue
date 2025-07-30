65ab237787497c11071f65d269467cd8c6aed193
DDEENNEERR <165697861+DDEENNEERR@users.noreply.github.com>
2025-07-30 13:36:28 -0300

    fhfvhfhfh

---
+++ imagem/script.js
@@ -0,0 +1,6 @@
+function inserirNome(){,
+    let nomeUsuario = prompt("Qual seu nome?");
+     let elemento = document. querySelector("#nome-Usuario");
+      console. log(elemento);
+       elemento. textContent = nomeUsuario;
+}
