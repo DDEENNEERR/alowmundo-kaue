f2216136b208850adec4116bcab49cc0b2085097
DDEENNEERR <165697861+DDEENNEERR@users.noreply.github.com>
2025-08-01 14:55:36 -0300

    grdtrdgewt

---
+++ script.js
@@ -0,0 +1,8 @@
+function inserirNome(){
+  let nomeUsuario = prompt("Qual seu nome");
+  let elemento = document.querySelector("#nome-usuario");
+  console.log(elemento)
+  elemento.textContent = nomeUsuario;
+}
+ const lista = document.querySelector('#lista');
+ 
