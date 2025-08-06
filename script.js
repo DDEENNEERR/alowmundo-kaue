c58ec247a52270a76eae310fdd2150498b57aa0b
DDEENNEERR <165697861+DDEENNEERR@users.noreply.github.com>
2025-08-06 13:33:22 -0300

    fgggg

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 function inserirNome(){
-  let nomeUsuario = prompt("Qual seu nome");
-  let elemento = document.querySelector("#nome-usuario");
-  console.log(elemento)
-  elemento.textContent = nomeUsuario;
+    let nomeUsuario = prompt("Qual seu nome");
+      let elemento = document.querySelector("#nome-usuario");
+      console.log(elemento)
+      elemento.textContent = nomeUsuario;
 }
- const lista = document.querySelector('#lista');
+const lista = document.querySelector('#lista');
+
+lista.textContent = linguagens[0];
+lista.textContent = linguagens[1];
+lista.textContent = linguagens[2];
 
 
-  lista.textContent = linguagens[0];
-  lista.textContent = linguagens[1];
-  lista.textContent = linguagens[2];
-  
-  let aluno1 ={
-  nome: 'Dener',
-  idade: '17',
-  anoLetivo: 'Ensino Médio',
-  materiasFavoritas: [EducaçãoFisica,artes,robotica]
-  }
-  
-    console.log(aluno1.nome);
-      console.log( aluno1.idade);
-        console.log(aluno1.materiasFavoritas);
-          console.log(aluno1.materiasFavoritas[1]);
-           
-          const.objeto = document.querySelector('#aluno');
-            objeto.contextContent = aluno1.nome;
-              objeto.contextContent = aluno1.idade;
-                objeto.contextContent = aluno1.materiasFavoritas[1];
-              
+let aluno1 ={
+  nome:'Dener',
+  idade: 17,
+  anoletivo:'Esino Médio',
+  materiasFavoritas:['Educação Física e Programação']
+}
+
+console.log(aluno1.nome);
+console.log(aluno1.idade);
+console.log(aluno1.materiasFavoritas);
+console.log(aluno1.materiasFavoritas[1]);
+
+const objeto = document.querySelector('#aluno');
+objeto.textContent = aluno1.nome;
+objeto.textContent = aluno1.idade;
+objeto.textContent = aluno1.materiasFavoritas[1];
